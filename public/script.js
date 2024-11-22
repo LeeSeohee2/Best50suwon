00cd9652a123fbc8be8e5d5498b640022c4ce787
LeeSeohee2 <141296423+LeeSeohee2@users.noreply.github.com>
2024-11-23 02:00:05 +0900

    Update script.js

--- public/script.js
+++ public/script.js
@@ -148,4 +148,5 @@ navLinksItems.forEach(item => {
 document.addEventListener('DOMContentLoaded', () => {
     initializeSectionVideo('events', 'discount'); // 이달의 행사 섹션 초기화
     initializeSectionVideo('contact-lens-reservation', 'networking'); // 콘택트렌즈 예약 섹션 초기화
+    initializeSectionVideo('new-glasses', 'tree'); // 새로 맞춘 안경 섹션 초기화
 });
